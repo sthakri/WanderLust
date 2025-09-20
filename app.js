1c16547e7a98d010fe97f8e477bfdceea082ee0a
sthakri <sthakritagya123@gmail.com>
2025-09-20 18:27:19 -0500

    feat(auth): initialize passport, add user/login

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
-const localStrategy = require("passport-local");
-const user = require("./models/user.js");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -40,11 +41,27 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
+
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "abc@gmail.com",
+//     username: " abcd",
+//   });
+//   let registeredUser = await User.register(fakeUser, "helloworld");
+//   res.send(registeredUser);
+// });
 
 main()
   .then(() => {
@@ -59,11 +76,12 @@ async function main() {
 }
 
 //Listing
-app.use("/listings", listings);
+app.use("/listings", listingRouter);
 
 //Review
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings/:id/reviews", reviewRouter);
 
+app.use("/", userRouter);
 //Error Handling
 app.all(/.*/, (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
